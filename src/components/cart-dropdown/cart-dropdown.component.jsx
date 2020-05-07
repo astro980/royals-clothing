ee916e0bb1742e9874dd8030933e8f2bc84100cc
astro980 <jurimar.langomez@gmail.com>
2020-05-07 04:27:40 -0600

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import CustomButton from '../custom-button/custom.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './cart-dropdown.styles.scss';
 
