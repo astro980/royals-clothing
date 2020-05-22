59f06b0825c73b511e9f71e3f1f85d0d3b6abb55
astro980 <jurimar.langomez@gmail.com>
2020-05-22 11:30:56 -0600

    re-initializing src folder

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyAsmHlxFv1Rps3nRXw0J9f9_hkkDPrBFlE",
+  authDomain: "royals-db.firebaseapp.com",
+  databaseURL: "https://royals-db.firebaseio.com",
+  projectId: "royals-db",
+  storageBucket: "royals-db.appspot.com",
+  messagingSenderId: "184169010670",
+  appId: "1:184169010670:web:90cc03a6173ba5f457eaff",
+  measurementId: "G-L85640FFBQ"
 };
 
 firebase.initializeApp(config);
